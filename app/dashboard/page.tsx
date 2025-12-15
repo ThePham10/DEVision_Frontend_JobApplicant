d3d1f0f75065af435bcd737525a9d29b56f8dc11
Hoang Dang <s3988412@rmit.edu.vn>
2025-12-15 10:57:25 +0700

    (feat) Add dashboard page with authentication guard to prevent the user who do not login to access the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,21 +2,22 @@
 
 import Header from "@/components/header/ui/header";
 import { useAuthStore } from "@/store/authStore";
+import { AuthGuard } from "@/components/auth-guard/authGuard";
 
 export default function Page() {
     const { user } = useAuthStore();
 
     return (
-        <>
+        <AuthGuard>
             <Header />
 
             <div className="flex flex-col min-h-screen items-center bg-[#f1f5f9]/30 p-10 gap-8">
-            <h1 className="text-3xl font-bold">Dashboard</h1>
-            <p>Welcome to your dashboard!</p>
-            <div>
-                {user?.email}
+                <h1 className="text-3xl font-bold">Dashboard</h1>
+                <p>Welcome to your dashboard!</p>
+                <div>
+                    {user?.email}
+                </div>
             </div>
-        </div>
-        </>
+        </AuthGuard>
     )
 }
---
+++ components/auth-guard/authGuard.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react"
+
+export const AuthGuard = ({children}: {children: React.ReactNode}) => {
+    const { isAuthenticated } = useAuthStore();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            router.push("/login");
+        }
+    }, [isAuthenticated, router])
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
+
+    return (<>{children}</>)
+}
