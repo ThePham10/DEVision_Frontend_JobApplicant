7ba2f8379485345c50c074eb2da43b3be5ba27d9
Hoang Dang <s3988412@rmit.edu.vn>
2026-01-06 23:26:31 +0700

    (fix) Update the index.ts in reusable-component file

--- components/reusable-component/index.ts
+++ components/reusable-component/index.ts
@@ -1,4 +1,3 @@
-// Default exports (need to be renamed)
 export { Badge } from "./Badge"
 export { Button } from "./Button"
 export { InfoCard } from "./InfoCard"
@@ -8,8 +7,6 @@ export { PremiumButton } from "./PremiumButton"
 export { PricingCard } from "./PricingCard"
 export { SecondaryButton } from "./SecondaryButton"
 export { ServiceCard } from "./ServiceCard"
-
-// Named exports
 export { AvatarFrame } from "./AvatarFrame"
 export { DEVisionLogoButton } from "./DEVisionLogoButton"
 export { EditButton } from "./EditButton"
@@ -17,8 +14,7 @@ export { NavBar } from "./NavBar"
 export { PopUpBox } from "./PopUpBox"
 export { Ribbon } from "./Ribbon"
 export { icons } from "./Icon"
-export { AuthGuard } from "./authGuard"
+export { AuthGuard } from "./AuthGuard"
 
-// Re-export types
 export type { InputProps, InputVariant, InputSize, InputState } from "./Input"
 export type { PopUpBoxProps } from "./PopUpBox"
